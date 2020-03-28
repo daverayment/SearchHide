b2fa8af14a8eb841310da5e1ce00ebcdc2fc8bac
Dave Rayment <dave.rayment@gmail.com>
2020-03-28 02:41:30 +0000

    Reworked structure of providers. Added Yahoo.

--- content.js
+++ content.js
@@ -1,61 +1,9 @@
 "use strict";
 /* global chrome */
 
-const SearchProvider = {
-    "DuckDuckGo": {
-        "ResultClass": "result__body links_main",
-        "ResultElements": function() {
-            return document.getElementsByClassName(this.ResultClass);
-        },
-        "Link": function(resultElement) {
-            const resultHrefs = resultElement.getElementsByClassName("result__a");
-            return resultHrefs.length === 0 ? "" : resultHrefs[0].href.toString();
-        }
-    },
-    "Bing": {
-        "ResultClass": "b_algo",
-        "ResultElements": function() {
-            return document.getElementsByClassName(this.ResultClass);
-        },
-        "Link": function(resultElement) {
-            const linkElements = resultElement.getElementsByTagName("a");
-            return linkElements.length > 0 ? linkElements[0].href : "";
-        }
-    },
-    "Google": {
-        "ResultClass": "rc",
-        "ResultElements": function() {
-            return document.getElementsByClassName("rc");
-        },
-        "Link": function(resultElement) {
-            const resultLinks = resultElement.getElementsByTagName("a");
-            return resultLinks.length === 0 ? "": resultLinks[0].origin;
-        }
-    },
-    "Unknown": {
-        "ResultClass": "",
-        "ResultElements": function() { return []; },
-        "Link": function() { return ""; }
-    }
-}
-
-let searchProvider = SearchProvider.Unknown;
-
-/**
- * Which search page is the user visiting?
- */
-function determineSearchProvider() {
-    const hostname = document.domain;
-
-    if (hostname.startsWith("duckduckgo.")) {
-        searchProvider = SearchProvider.DuckDuckGo;
-    }
-    else if (hostname.includes(".bing.")) {
-        searchProvider = SearchProvider.Bing;
-    }
-    else if(hostname.includes(".google.")) {
-        searchProvider = SearchProvider.Google;
-    }
+function Provider(name, resultClass) {
+    this.name = name;
+    this.resultClass = resultClass;
 }
 
 /**
@@ -63,21 +11,75 @@ function determineSearchProvider() {
  * page elements which require processing, i.e. those which have not already
  * been hidden.
  */
-function getResultElements() {
-    return Array.from(searchProvider.ResultElements())
+Provider.prototype.getResultElements = function () {
+    return Array.from(document.getElementsByClassName(this.resultClass))
         .filter((element) => !element.classList.contains("hidden"));
-
-    // return Array.from(document.getElementsByClassName(searchProvider.ResultClassName))
-    //     .filter((element) => !element.classList.contains("hidden"));
-}
+};
 
 /**
  * Locate and extract the HREF from a single search page result.
  * @param {Element} resultElement - The search page result element.
+ * @param {string} [linkClass] - The link element's class. If present, this will be used instead of just looking for anchor tags.
+ * @returns {string} - The URL, or "" if not found.
  */
-function getLinkFromResult(resultElement) {
-    return searchProvider.Link(resultElement);
+Provider.prototype.getLinkFromResultElement = (resultElement, linkClass) => {
+    let resultHrefs = undefined;
+    if (typeof(linkClass) === "undefined") {
+        resultHrefs = resultElement.getElementsByTagName("a");
+    }
+    else {
+        resultHrefs = resultElement.getElementsByClassName(linkClass);
+    }
+    return resultHrefs.length === 0 ? "" : resultHrefs[0].href.toString();
+};
+
+Provider.prototype.getResultFromHideLink = function (linkElement) {
+    const result = linkElement.parentElement.getElementsByClassName(this.resultClass)[0];
+    return result;
 }
+
+/**
+ * Replace a result with 'Result hidden' text.
+ * @param {Element} result - The result element.
+ */
+Provider.prototype.hideResult = (result) => {
+    const noteDiv = createResultHiddenElement(result);
+
+    // TODO: messy
+    if (this.name === "Bing") {
+        result.parentElement.insertBefore(noteDiv, result);
+    }
+    else {
+        result.parentElement.prepend(noteDiv);
+    }
+    result.classList.add("hidden");
+}
+
+let searchProvider = function () {
+    const hostname = document.domain;
+    if (hostname.startsWith("duckduckgo.")) {
+        return new Provider("Duck Duck Go", "result__body links_main");
+    }
+    else if (hostname.includes(".bing.")) {
+        return new Provider("Bing", "b_algo");
+    }
+    else if (hostname.includes(".google.")) {
+        return new Provider("Google", "rc");
+    }
+    else if(hostname.includes("search.yahoo.com")) {
+        let p = new Provider("Yahoo", "algo-sr");
+        p.getResultElements = () => {
+            return Array.from(document.querySelectorAll("div.algo-sr"))
+                .map(x => x.parentElement);
+        }
+        return p;
+    }
+    else {
+        return new Provider("Unknown", "");
+    }
+}();
+
+Object.freeze(searchProvider);
 
 /**
  * Determine whether a result should be hidden on the page.
@@ -93,41 +95,29 @@ function shouldHide(link, hiddenSites) {
  * available to process.
  */
 async function waitForElements() {
-    while(getResultElements().length === 0) {
+    while(searchProvider.getResultElements().length === 0) {
         await new Promise(r => setTimeout(r, 500));
     }
-    return getResultElements();
+    return searchProvider.getResultElements();
 }
 
-/**
- * Replace a result with 'Result hidden' text.
- * @param {Element} result - The result element.
- */
-function hideResult(result) {
+function createResultHiddenElement(result) {
     const noteDiv = document.createElement("div");
     noteDiv.innerHTML = chrome.i18n.getMessage("resultHidden") +
-        ` (${new URL(getLinkFromResult(result)).hostname})`;
+        ` (${new URL(searchProvider.getLinkFromResultElement(result)).hostname})`;
     noteDiv.classList.add('hide-link');
     // When the 'Result hidden' text is clicked, unhide the
     // result.
-    noteDiv.addEventListener("click", function(ev) {
-        // TODO: messy
-        const target = Object.is(searchProvider, SearchProvider.Bing) ?
-            ev.target.nextElementSibling :
-            ev.target.parentElement.getElementsByClassName(searchProvider.ResultClass)[0];
-        target.classList.remove("hidden");
-        target.classList.add("unhidden");
-        ev.currentTarget.remove();
-        ev.stopPropagation();
-    });
-    // TODO: messy
-    if (Object.is(searchProvider, SearchProvider.Bing)) {
-        result.parentElement.insertBefore(noteDiv, result);
-    }
-    else {
-        result.parentElement.prepend(noteDiv);
-    }
-    result.classList.add("hidden");
+    noteDiv.addEventListener("click", unhideResult);
+    return noteDiv;
+}
+
+function unhideResult(ev) {
+    const resultElement = searchProvider.getResultFromHideLink(ev.target);
+    resultElement.classList.remove("hidden");
+    resultElement.classList.add("unhidden");
+    ev.currentTarget.remove();
+    ev.stopPropagation();
 }
 
 function processSearchPage() {
@@ -138,9 +128,9 @@ function processSearchPage() {
             // Get the user's preferences from storage
             chrome.storage.sync.get(["hidden_sites"], (stored) => {
                 for (const result of resultElements) {
-                    const link = getLinkFromResult(result);
+                    const link = searchProvider.getLinkFromResultElement(result);
                     if (shouldHide(link, stored.hidden_sites)) {
-                        hideResult(result);
+                        searchProvider.hideResult(result);
                     }
                     else {
                         result.classList.remove("hidden");
@@ -157,9 +147,6 @@ function processSearchPage() {
     );
 }
 
-determineSearchProvider();
-processSearchPage();
-
 /**
  * Add our 'Hide' link to a specific search result.
  * @param {Element} result - The search result element to add our link to.
@@ -169,7 +156,7 @@ function addLink(result) {
     a.appendChild(document.createTextNode(chrome.i18n.getMessage("hidePrompt")));
     a.title = chrome.i18n.getMessage("hidePromptTitle");
     a.href = "javascript:;";
-    const resultLink = searchProvider.Link(result);
+    const resultLink = searchProvider.getLinkFromResultElement(result);
     if (resultLink.length === 0) {
         return;
     }
@@ -208,3 +195,5 @@ chrome.storage.onChanged.addListener((changes) => {
 
     processSearchPage();
 });
+
+processSearchPage();
